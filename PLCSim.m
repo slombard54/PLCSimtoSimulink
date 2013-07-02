b54b0ab430097684c8112d65c49261a3cdb8d544
Scott Lombard <slombard@kingindustries.com>
2013-07-02 11:49:50 -0400

    Initial commit

---
+++ PLCSim.m
@@ -0,0 +1,109 @@
+function varargout = PLCSim(varargin)
+% PLCSIM MATLAB code for PLCSim.fig
+%      PLCSIM, by itself, creates a new PLCSIM or raises the existing
+%      singleton*.
+%
+%      H = PLCSIM returns the handle to a new PLCSIM or the handle to
+%      the existing singleton*.
+%
+%      PLCSIM('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in PLCSIM.M with the given input arguments.
+%
+%      PLCSIM('Property','Value',...) creates a new PLCSIM or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before PLCSim_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to PLCSim_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help PLCSim
+
+% Last Modified by GUIDE v2.5 01-Jul-2013 22:47:12
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @PLCSim_OpeningFcn, ...
+                   'gui_OutputFcn',  @PLCSim_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+NET.addAssembly ('C:\Users\slombard.KINGDOM\Documents\Visual Studio 2010\Projects\PLCSimConnector\PLCSimConnector\bin\Debug\PLCSimConnector.dll');
+
+
+% --- Executes just before PLCSim is made visible.
+function PLCSim_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to PLCSim (see VARARGIN)
+
+% Choose default command line output for PLCSim
+handles.output = hObject;
+
+proj = PLCSimConnector.PCS7Project('C:\Program Files (x86)\SIEMENS\STEP7\S7Proj\KING_M_1\MID_CTRL\MID_CTRL.s7p');
+handles.project = proj;
+% Update handles structure
+guidata(hObject, handles);
+
+set (handles.popupmenu1, 'String', transpose(cell(proj.GetOutputImageSymbols())));
+% UIWAIT makes PLCSim wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = PLCSim_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on selection change in popupmenu1.
+function popupmenu1_Callback(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from popupmenu1
+
+proj = handles.project;
+contents = cellstr(get(hObject,'String'));
+v = contents{get(hObject,'Value')};
+entry = proj.PCS7SymbolTable.GetEntryFromSymbol(v);
+v = char(entry.OperandIEC);
+set(handles.addressText, 'String', char(entry.OperandIEC));
+set(handles.dataTypeText, 'String', char(entry.DataType));
+set(handles.descriptionText, 'String', char(entry.Comment));
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
---
+++ PLCSim_Connect.m
@@ -0,0 +1,179 @@
+function PLCSim_Connect(block)
+%MSFUNTMPL_BASIC A Template for a Level-2 MATLAB S-Function
+%   The MATLAB S-function is written as a MATLAB function with the
+%   same name as the S-function. Replace 'msfuntmpl_basic' with the 
+%   name of your S-function.
+%
+%   It should be noted that the MATLAB S-function is very similar
+%   to Level-2 C-Mex S-functions. You should be able to get more
+%   information for each of the block methods by referring to the
+%   documentation for C-Mex S-functions.
+%
+%   Copyright 2003-2010 The MathWorks, Inc.
+
+%%
+%% The setup method is used to set up the basic attributes of the
+%% S-function such as ports, parameters, etc. Do not add any other
+%% calls to the main body of the function.
+%%
+setup(block);
+
+%endfunction
+
+%% Function: setup ===================================================
+%% Abstract:
+%%   Set up the basic characteristics of the S-function block such as:
+%%   - Input ports
+%%   - Output ports
+%%   - Dialog parameters
+%%   - Options
+%%
+%%   Required         : Yes
+%%   C-Mex counterpart: mdlInitializeSizes
+%%
+function setup(block)
+
+% Register number of ports
+block.NumInputPorts  = 0;
+block.NumOutputPorts = 0;
+
+set_param(block.BlockHandle, 'Priority', '0');
+
+% Setup port properties to be inherited or dynamic
+%block.SetPreCompInpPortInfoToDynamic;
+%block.SetPreCompOutPortInfoToDynamic;
+
+% Override input port properties
+%block.InputPort(1).Dimensions        = 1;
+%block.InputPort(1).DatatypeID  = 0;  % double
+%block.InputPort(1).Complexity  = 'Real';
+%block.InputPort(1).DirectFeedthrough = true;
+
+% Override output port properties
+%block.OutputPort(1).Dimensions       = 1;
+%block.OutputPort(1).DatatypeID  = 0; % double
+%block.OutputPort(1).Complexity  = 'Real';
+%block.OutputPort(1).SamplingMode = 'sample';
+
+% Register parameters
+block.NumDialogPrms     = 2;
+block.DialogPrmsTunable = {'Nontunable','Nontunable'};
+
+% Register sample times
+%  [0 offset]            : Continuous sample time
+%  [positive_num offset] : Discrete sample time
+%
+%  [-1, 0]               : Inherited sample time
+%  [-2, 0]               : Variable sample time
+block.SampleTimes = [0 0];
+
+% Specify the block simStateCompliance. The allowed values are:
+%    'UnknownSimState', < The default setting; warn and assume DefaultSimState
+%    'DefaultSimState', < Same sim state as a built-in block
+%    'HasNoSimState',   < No sim state
+%    'CustomSimState',  < Has GetSimState and SetSimState methods
+%    'DisallowSimState' < Error out when saving or restoring the model sim state
+block.SimStateCompliance = 'DefaultSimState';
+
+NET.addAssembly ('C:\Users\slombard.KINGDOM\Documents\Visual Studio 2010\Projects\PLCSimConnector\PLCSimConnector\bin\Debug\PLCSimConnector.dll');
+Sim = PLCSimConnector.PLCSim;
+set_param(block.BlockHandle, 'UserData', Sim);
+
+%% -----------------------------------------------------------------
+%% The MATLAB S-function uses an internal registry for all
+%% block methods. You should register all relevant methods
+%% (optional and required) as illustrated below. You may choose
+%% any suitable name for the methods and implement these methods
+%% as local functions within the same file. See comments
+%% provided for each function for more information.
+%% -----------------------------------------------------------------
+
+block.RegBlockMethod('CheckParameters',         @CheckPrms);
+block.RegBlockMethod('ProcessParameters',       @ProcessPrms);
+block.RegBlockMethod('PostPropagationSetup',    @DoPostPropSetup);
+block.RegBlockMethod('InitializeConditions', @InitializeConditions);
+block.RegBlockMethod('Start', @Start);
+block.RegBlockMethod('Terminate', @Terminate); % Required
+
+%end setup
+
+function CheckPrms(block)
+  projectName = block.DialogPrm(1).Data;
+  plcName = block.DialogPrm(2).Data;
+
+  if ~strcmp(projectName, '') && exist(projectName, 'file') == 0 
+    error('PLCSim:Param', 'Project File %s does not exist', projectName)
+  end
+  
+  if ~strcmp(plcName, '') && exist(plcName, 'file') == 0
+    error('PLCSim:Param', 'PLC File %s does not exist', plcName)
+  end
+
+%%
+%% PostPropagationSetup:
+%%   Functionality    : Setup work areas and state variables. Can
+%%                      also register run-time methods here
+%%   Required         : No
+%%   C-Mex counterpart: mdlSetWorkWidths
+%%
+function DoPostPropSetup(block)
+block.NumDworks = 1;
+  
+  block.Dwork(1).Name            = 'x1';
+  block.Dwork(1).Dimensions      = 1;
+  block.Dwork(1).DatatypeID      = 0;      % double
+  block.Dwork(1).Complexity      = 'Real'; % real
+  block.Dwork(1).UsedAsDiscState = true;
+  
+    %% Register all tunable parameters as runtime parameters.
+  block.AutoRegRuntimePrms;
+
+function ProcessPrms(block)
+
+  block.AutoUpdateRuntimePrms;
+ 
+%endfunction
+
+
+%%
+%% InitializeConditions:
+%%   Functionality    : Called at the start of simulation and if it is 
+%%                      present in an enabled subsystem configured to reset 
+%%                      states, it will be called when the enabled subsystem
+%%                      restarts execution to reset the states.
+%%   Required         : No
+%%   C-MEX counterpart: mdlInitializeConditions
+%%
+function InitializeConditions(block)
+block.Dwork(1).Data = 0;
+%end InitializeConditions
+
+
+%%
+%% Start:
+%%   Functionality    : Called once at start of model execution. If you
+%%                      have states that should be initialized once, this 
+%%                      is the place to do it.
+%%   Required         : No
+%%   C-MEX counterpart: mdlStart
+%%
+function Start(block)
+Sim = get_param(block.BlockHandle, 'UserData');
+Sim.Connect();
+block.Dwork(1).Data = 0;
+%Sim = get_param(block.BlockHandle, 'UserData');
+
+%end Start
+
+
+%%
+%% Terminate:
+%%   Functionality    : Called at the end of simulation for cleanup
+%%   Required         : Yes
+%%   C-MEX counterpart: mdlTerminate
+%%
+function Terminate(block)
+Sim = get_param(block.BlockHandle, 'UserData');
+Sim.Disconnect
+%end Terminate
+
